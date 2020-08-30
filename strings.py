5ae32e2d8b3ac6b947ebeaa75d78e8b65612ed68
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-30 19:30:48 -0400

    renamed file

---
+++ strings.py
@@ -0,0 +1,36 @@
+# print('hello world')
+
+# print("   /__|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+# character_name = "TONIA SABA"
+# character_age = "27"
+# is_Female = True
+# print("there once was a man named " + character_name + ",")
+
+# character_name = "TSABZ"
+# character_age = "27"
+# print("she is a software engineer and is " + character_age + ",")
+# print(" " + character_name + " just got a job for 70,000")
+
+# creates a new line
+print("Giraffe\nAcademy")
+
+# prints out a quotion mark
+print("Giraffe\"Academy") 
+
+phrase = "Tonia will travel the world with a dj collective"
+print(phrase + " very soon")
+
+# converts to lowercase & uppercase
+print(phrase.lower())
+print(phrase.upper())
+
+print(phrase.isupper()) #false because the origin phrase isn't upper
+print(phrase.upper().isupper()) #true, because it converts then checks
+
+print(len(phrase)) #checks for length in string
+
+print(phrase[0]) # T first letter in the string
