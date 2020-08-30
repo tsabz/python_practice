b0ff455ad6461cd9c08ea02f24826badcd0182da
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-30 19:49:13 -0400

    numbers practice

--- numbers.py
+++ numbers.py
@@ -1,3 +1,6 @@
+from math import * #math module. gives us access to a lot more math functions
+
+
 print(48) #48
 print(2.94283492) #2.94283492
 print(-2.94283492) # -2.94283492
@@ -21,3 +24,14 @@ print(my_num + 5) #10
 print(str(my_num)) # "5"
 
 print(str(my_num) + " my favorite number") # "5 my favorite number"
+
+my_num = -5
+print(abs(my_num)) #5
+
+print(pow(3, 2)) #3 raise to the power of two 3*3, returns 9
+print(max(4, 6)) # tells us which number is higher. returns 6
+print(min(4, 6)) #4
+print(round(3.7)) #4
+print(floor(3.7)) #3 rounds the number down no matter what
+print(ceil(3.2)) #4 rounds the number up no matter what
+print(sqrt(36)) #6.0
