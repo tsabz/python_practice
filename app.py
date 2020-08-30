5503b9e81c2e83a4b59d417142351afa0e2ad24f
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-30 19:12:14 -0400

    initialized git'

---
+++ app.py
@@ -0,0 +1,16 @@
+# print('hello world')
+
+# print("   /__|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+# character_name = "TONIA SABA"
+# character_age = "27"
+# is_Female = True
+# print("there once was a man named " + character_name + ",")
+
+# character_name = "TSABZ"
+# character_age = "27"
+# print("she is a software engineer and is " + character_age + ",")
+# print(" " + character_name + " just got a job for 70,000")
