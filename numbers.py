65e9c51f8362b43bd2aa9143791cda7b4522d06d
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-30 19:42:57 -0400

    created numbers file

---
+++ numbers.py
@@ -0,0 +1,23 @@
+print(48) #48
+print(2.94283492) #2.94283492
+print(-2.94283492) # -2.94283492
+
+print(3 + 4.5) #7.5
+print(3 / 4.5) #0.6666666666666666
+
+print(3 * 4 + 5) #17
+
+print(3 * (4 + 5)) #27
+
+print (10 % 3) #spits out the remainder which is 1
+
+print (10 % 2) #0
+
+my_num = 5
+
+print(my_num) #5
+print(my_num + 5) #10
+
+print(str(my_num)) # "5"
+
+print(str(my_num) + " my favorite number") # "5 my favorite number"
