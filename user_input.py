77fc392bdcfb34aca89875803d141f166239680d
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-30 19:57:03 -0400

    created userinput file

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ") #taking user input and storing to variable name
+age = input("Enter your AGE: ")
+print("hello " + name + "! YOU ARE " + age)
+# Enter your name: TONIA
+# hello TONIA!
+
+# Enter your name: TONIA
+# Enter your AGE: 27
+# hello TONIA! YOU ARE 27!
