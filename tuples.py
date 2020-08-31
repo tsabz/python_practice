9c369f96261ac0bc9e3d61c45f894e79b4591aae
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-31 16:12:33 -0400

    tuple file notes done

---
+++ tuples.py
@@ -0,0 +1,10 @@
+#a tuple cant be changed or modified 
+
+coordinates = (4, 5)
+
+coordinates[1] = 10 #'tuple' object does not support item assignment
+
+print(coordinates[1]) #5
+
+
+#lists you are able to change , tuples you are not
