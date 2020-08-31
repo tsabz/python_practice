214377bbb9f1372c03eef0f85951cb93d234305c
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-30 20:13:09 -0400

    finished madlib

---
+++ mad_libs.py
@@ -0,0 +1,15 @@
+# print("Roses are red")
+# print("Violets are blue")
+# print("I love you")
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+girl_name = input("Enter a girl name: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + girl_name)
+
+# Roses are pink
+# Dishes are blue
+# I love Nicol
