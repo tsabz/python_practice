a362ba879abe39f6be052cf1b00f8490ce853c2d
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-31 15:45:08 -0400

    list notes

---
+++ lists.py
@@ -0,0 +1,9 @@
+
+friends = ["Kevin" , "karen", "Jim"]
+
+print(friends)
+print(friends[0]) #Kevin
+print(friends[-2]) #Karen
+print(friends[1:]) #['karen', 'Jim']
+print(friends[1:3]) #['karen', 'Jim']
+
