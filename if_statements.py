d43e8ef168f8afba06da792c3a3d01ac0096043e
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-31 19:48:50 -0400

    finished if statements

--- if_statements.py
+++ if_statements.py
@@ -1,8 +1,12 @@
 
-is_male = False
-is_tall = True
+is_male = True
+is_tall = False
 
 if is_male and is_tall:
     print("you are a male")
+elif is_male and not(is_tall):
+    print("you are not a male and you are not tall")
+elif not(is_male) and is_tall:
+    print("blah blah")
 else:
     print("you are a female")
