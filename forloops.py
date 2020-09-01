fbaf00cb591c0f1565d91c19c534b35d3136ffc0
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-09-01 16:52:19 -0400

    for loops done

---
+++ forloops.py
@@ -0,0 +1,23 @@
+
+friends = ["Jim", "Karen", "Kevin"]
+
+
+print("**********************")
+for asdf in friends:
+    print(asdf)
+
+for index in range(5):
+    if index == 0:
+        print("first iteration")
+    else:
+        print("not first")
+
+# for num in "2345":
+#     print(num)
+
+
+# for index in range(10):
+#     print(index)
+
+# for index in range(3, 10):
+#     print(index)
