51878a18f1a490cd3a59ec4e5eeae48d670c82db
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-09-01 12:19:38 -0400

    if statement and comparisions notes finished

--- if_statements.py
+++ if_statements.py
@@ -1,12 +1,22 @@
 
-is_male = True
-is_tall = False
+# is_male = True
+# is_tall = False
 
-if is_male and is_tall:
-    print("you are a male")
-elif is_male and not(is_tall):
-    print("you are not a male and you are not tall")
-elif not(is_male) and is_tall:
-    print("blah blah")
-else:
-    print("you are a female")
+# if is_male and is_tall:
+#     print("you are a male")
+# elif is_male and not(is_tall):
+#     print("you are not a male and you are not tall")
+# elif not(is_male) and is_tall:
+#     print("blah blah")
+# else:
+#     print("you are a female")
+
+
+def is_male(boolean): 
+    if is_male:
+        return "you are a male!!!!!" 
+
+results = is_male(True)
+
+print(results)
+       
