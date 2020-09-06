49229453a14ebe65393774777dae722052b1daab
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-09-06 16:53:51 -0400

    inheritance py

---
+++ inheritance.py
@@ -0,0 +1,9 @@
+from Chinese_Chef import ChineseChef
+from Chef import Chef
+
+chef = Chef()
+chef.make_chicken()
+
+chienseChef = ChineseChef()
+chienseChef.make_chicken()
+
---
+++ object_functions.py
@@ -0,0 +1,6 @@
+from classes_objects import Student
+
+student1 = Student("Oscar","Accounting", 3.1, False )
+student2 = Student("Tonia","Computer Science", 3.1, False )
+
+print(student1.on_honor_roll())
